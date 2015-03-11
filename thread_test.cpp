a80f943ce24b378c59a4a673237ef2dac8f98463
Brittney Russell <bsrussell15@gmail.com>
2015-03-11 16:08:47 -0700

    Starting test driver

--- thread_test.cpp
+++ thread_test.cpp
@@ -19,5 +19,77 @@
  *
  ***********************************************/
 
+#include "q.h"
 #include "threads.h"
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/********************
+ * Gobal Variables
+ ********************/
+Queue RunQ;
+int globalInt = 0;
+
+/********************
+ * Function Prototypes
+ ********************/
+void f1();
+void f2();
+
+
+int main()
+{
+	struct TCB_t *newTCB;
+
+	/*************************
+	 * Function Pointers
+	 ************************/
+	void (*f1)();
+	void (*f2)();
+
+	/*************************
+	 * Initialize Threads
+	 ************************/
+	start_thread(f1, newTCB);
+	start_thread(f2, newTCB);
+
+	run();
+}
+
+void f1()
+{
+	int localInt = 0;
+	while(1)
+	{
+		printf("f1 %d", localInt);
+		yield();
+		localInt++;
+
+		printf("f2 %d", localInt);
+		yield();
+
+		sleep();
+	}
+}
+
+void f2()
+{
+	int localInt = 0;
+
+	while(1)
+	{
+		printf("f2 %d", localInt);
+		yield();
+		localInt++;
+
+		printf("f1 %d", localInt);
+
+		yield();
+
+		sleep();
+	}
+}
 
